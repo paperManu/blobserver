f4c6d86af7fcbd5ee4b864b59267c333a8cfd44e
Emmanuel Durand <emmanueldurand@gmail.com>
2013-03-29 19:11:13 -0400

    SVM classifier with HOG starting to look good

--- include/descriptor_hog.h
+++ include/descriptor_hog.h
@@ -8,13 +8,13 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * switcher is distributed in the hope that it will be useful,
+ * blobserver is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with switcher.  If not, see <http://www.gnu.org/licenses/>.
+ * along with blobserver.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -42,13 +42,19 @@ class Descriptor_Hog
         Descriptor_Hog();
 
         /**
+         * Specifies the image from which to create the descriptor(s).
+         * Also, computes the oriented gradient at each pixel.
+         * \param pImage Image
+         */
+        void setImage(const cv::Mat& pImage);
+
+        /**
          * Gets the descriptor of the specified image, with the given cropping parameters
          * if they are set. Otherwise, default parameters set with setCrop() are used.
-         * \param pImage Image for which to create a descriptor
-         * \param pCropRect Rectangle defining the region to crop
-         * \return A vector of floats containing all components of the descriptor
+         * \param pPos Position of the region (defined by the hog parameters) from where to create the classifier
+         * \return A vector of floats containing all components of the descriptor. Size is 0 if there was not enough space to build the whole descriptor
          */
-        vector<float> getDescriptor(const cv::Mat& pImage, const cv::Rect_<int> pCropRect = cv::Rect_<int>()) const;
+        vector<float> getDescriptor(const cv::Point_<int> pPos = cv::Point_<int>()) const;
 
         /**
          * Sets the default crop and margin to use
@@ -58,60 +64,74 @@ class Descriptor_Hog
          * \param pTransV Horizontal translation of the crop
          * \param pMargin Margin to consider inside the cropped selection
          */
-        void setRoi(cv::Rect_<int> pCropRect, uint pMargin = 0);
+        void setRoi(cv::Rect_<int> pCropRect, int pMargin = 0);
 
         /**
          * Specifies various parameters for the histogram of oriented gradients creation.
          * See [Dalal et al. 2005] for more information about each one of them.
-         * \param pBlockSize Number of cells per block, in both dimensions (default: 2x2)
+         * \param pDescriptorSize Size (in cells) of a descriptor (default: 8x16)
+         * \param pBlockSize Number of cells per block, in both dimensions (default: 3x3)
          * \param pCellSize Number of pixels per cell, in both dimensions (default: 8x8)
          * \param pBinsPerCell Number of bin (corresponding to orientations) stored in each cell (default: 9)
          * \param pSigned Set to true if signed gradients are desired, false otherwise (default: false)
          */
-        void setHogParams(const cv::Size_<uint> pBlockSize, const cv::Size_<uint> pCellSize, const unsigned int pBinsPerCell, const bool pSigned = false);
+        void setHogParams(const cv::Size_<int> pDescriptorSize, const cv::Size_<int> pBlockSize, const cv::Size_<int> pCellSize, const unsigned int pBinsPerCell, const bool pSigned = false);
 
     private:
+        // Input image
+        cv::Mat _image;
+        // Gradients orientation and value image
+        cv::Mat _gradients;
+
         // Crop parameters
         bool _doCrop;
         cv::Rect_<int> _cropRect;
         uint _margin;
 
         // R-HOG parameters (see [Dalal et al. 2005])
+        cv::Size_<int> _descriptorSize;
         cv::Size_<int> _blockSize;
         cv::Size_<int> _cellSize;
         uint _binsPerCell;
         bool _signed;
+
+        float _epsilon; // A small value, used for normalization.
+
+        // Method to compute the norm of a descriptor
+        float getDescriptorNorm(vector<float> pDescriptor) const;
 };
 
 /*************/
 Descriptor_Hog::Descriptor_Hog():
-    _doCrop(false),
-    _margin(0)
+    _doCrop(false)
 {
-    _blockSize.width = 2;
-    _blockSize.height = 2;
+    _descriptorSize.width = 8;
+    _descriptorSize.height = 16;
+    _blockSize.width = 3;
+    _blockSize.height = 3;
     _cellSize.width = 8;
     _cellSize.height = 8;
     _binsPerCell = 9;
     _signed = false;
+
+    _margin = max(_cellSize.width, _cellSize.height) * max(_blockSize.width/2, _blockSize.height/2);
+
+    _epsilon = 0.01f;
 }
 
 /*************/
-vector<float> Descriptor_Hog::getDescriptor(const cv::Mat& pImage, cv::Rect_<int> pCropRect) const
+void Descriptor_Hog::setImage(const cv::Mat& pImage)
 {
-    vector<float> descriptor;
+    cv::Mat tmpImage = pImage.clone();
 
-    cv::Mat roi;
-    if (pCropRect.width && pCropRect.height)
-        roi = cv::Mat(pImage, pCropRect);
-    else if (_doCrop)
-        roi = cv::Mat(pImage, _cropRect);
+    if (_doCrop)
+        _image = cv::Mat(tmpImage, _cropRect);
     else
-        roi = pImage;
+        _image = tmpImage;
 
-    cv::Mat gradientsH(roi.rows, roi.cols, pImage.type());
-    cv::Mat gradientsV(roi.rows, roi.cols, pImage.type());
-    uint cn = CV_MAT_CN(roi.type());
+    cv::Mat gradientsH(_image.rows, _image.cols, pImage.type());
+    cv::Mat gradientsV(_image.rows, _image.cols, pImage.type());
+    uint cn = CV_MAT_CN(_image.type());
 
     // Get the horizontal and vertical gradients
     cv::Mat kernelH(1, 3, CV_32FC1);
@@ -120,42 +140,168 @@ vector<float> Descriptor_Hog::getDescriptor(const cv::Mat& pImage, cv::Rect_<int
     kernelH.at<float>(0, 2) = 1.f;
     cv::Mat kernelV = kernelH.t();
 
-    cv::filter2D(roi, gradientsH, -1, kernelH);
-    cv::filter2D(roi, gradientsV, -1, kernelV);
+    cv::filter2D(_image, gradientsH, CV_32F, kernelH);
+    cv::filter2D(_image, gradientsV, CV_32F, kernelV);
 
     // Compute the oriented gradient for each pixel
-    for (uint x = 0; x < roi.cols; ++x)
-    {
-        for (uint y = 0; y < roi.rows; ++y)
-        {
-        }
-    }
+    cv::Mat channelsH[cn];
+    cv::Mat channelsV[cn];
+    cv::split(gradientsH, channelsH);
+    cv::split(gradientsV, channelsV);
 
-    // For each cell
-    uint cNbrH = (roi.cols - 2*_margin) % _cellSize.width;
-    uint cNbrV = (roi.rows - 2*_margin) % _cellSize.height;
-    for (uint indexH = 0; indexH < cNbrH; ++indexH)
+    _gradients = cv::Mat(_image.rows, _image.cols, CV_8UC2);
+    int rowLength = tmpImage.cols;
+    for (uint x = 0; x < _image.cols; ++x)
     {
-        for (uint indexV = 0; indexV < cNbrV; ++indexV)
+        for (uint y = 0; y < _image.rows; ++y)
         {
+            float hValue = 0.f;
+            float vValue = 0.f;
+            // We get the maximum gradient over the image
+            // TODO: check if we should not separate completely all channels
+            for (int i = 0; i < cn; ++i)
+            {
+                if (abs(channelsH[i].at<float>(y, x)) > abs(hValue))
+                    hValue = channelsH[i].at<float>(y, x);
+                vValue = max(vValue, abs(channelsV[i].at<float>(y, x)));
+            }
+            float length = sqrtf(hValue*hValue + vValue*vValue);
+
+            float angle;
+            if (length == 0)
+            {
+                angle = 0;
+            }
+            else
+            {
+                hValue /= length;
+                angle = acos(hValue);
+            }
+
+            _gradients.at<cv::Vec2b>(y, x)[0] = (uint)(angle / CV_PI * 180.f);
+            _gradients.at<cv::Vec2b>(y, x)[1] = (uint)length;
+            // TODO: add signed oriented gradients
         }
     }
+}
+
+/*************/
+vector<float> Descriptor_Hog::getDescriptor(cv::Point_<int> pPos) const
+{
+    vector<float> descriptor;
+
+    // Parameters for the kmeans alg used for bins creation
+    cv::TermCriteria criteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 5, 0.01); // TODO: Temporary defaults values
+
+    // Check if we have enough room to build a complete descriptor
+    int roiSizeH = _descriptorSize.width * _cellSize.width + _margin * 2;
+    int roiSizeV = _descriptorSize.height * _cellSize.height + _margin * 2;
+    if (pPos.x + roiSizeH >= _image.cols || pPos.y + roiSizeV >= _image.rows)
+        return descriptor;
+    // If position is to close to the border (no margin), we don't have enough room either
+    if (pPos.x < _margin || pPos.y < _margin)
+        return descriptor;
+    
+    // For each cell, we compute its descriptor
+    uint windowH = _descriptorSize.width + (_blockSize.width-1); // (_blockSize-1) is added to handle margin
+    uint windowV = _descriptorSize.height + (_blockSize.height-1); // (_blockSize-1) is added to handle margin
+    vector< vector<float> > cellsDescriptor;
+    for (uint cellH = 0; cellH < windowH; ++cellH)
+        for (uint cellV = 0; cellV < windowV; ++cellV)
+        {
+            cv::Point_<int> topLeft;
+            topLeft.x = cellH * _cellSize.width + pPos.x;
+            topLeft.y = cellV * _cellSize.height + pPos.y;
+            
+            // Creation of the histogram
+            // TODO: Add bilinear interpolation of bin centers and orientation
+            vector<float> cellDescriptor;
+            cellDescriptor.assign(_binsPerCell, 0);
+            cv::Mat hist = cv::Mat::zeros(1, _binsPerCell, CV_16UC1);
+            for (int x = 0; x < _cellSize.width; ++x)
+                for (int y = 0; y < _cellSize.height; ++y)
+                {
+                    int range = _signed ? 360 : 180;
+                    int index = _gradients.at<cv::Vec2b>(topLeft.y + y, topLeft.x + x)[0] / (range/_binsPerCell);
+                    cellDescriptor[index] += _gradients.at<cv::Vec2b>(topLeft.y + y, topLeft.x + x)[1];
+                }
+
+            cellsDescriptor.push_back(cellDescriptor);
+        }
+
+    // We have all cells descriptors. Now we normalize them to create the global descriptor
+    // TODO: include a Gaussian spatial window over the block
+    for (int cellH = 0; cellH < _descriptorSize.width; ++cellH)
+        for (int cellV = 0; cellV < _descriptorSize.height; ++cellV)
+        {
+            int blockCenterH = cellH + _blockSize.width/2;
+            int blockCenterV = cellV + _blockSize.height/2;
+
+            vector<float> descriptorVector;
+            descriptorVector.assign(_binsPerCell, 0);
+            // We calculate the norm of the descriptor over the whole block
+            // So we go over all the cells for the current block
+            for (int i = -_blockSize.width/2; i < _blockSize.width/2; ++i)
+                for (int j = -_blockSize.height/2; j < _blockSize.height/2; ++j)
+                {
+                    int index = blockCenterH+i + (blockCenterH+j)*windowV;
+                    for (int orientation = 0; orientation < _binsPerCell; ++orientation)
+                    {
+                        descriptorVector[orientation] += cellsDescriptor[index][orientation];
+                    }
+                }
+
+            // We need the norm for the current block descriptor
+            float norm = getDescriptorNorm(descriptorVector);
+
+            // And we scale the whole block, to get the descriptor for the current cell
+            for (int i = 0; i < _binsPerCell; ++i)
+                descriptor.push_back(descriptorVector[i] / norm);
+        }
 
     return descriptor;
 }
 
 /*************/
-void Descriptor_Hog::setRoi(cv::Rect_<int> pCropRect, uint pMargin)
+void Descriptor_Hog::setRoi(cv::Rect_<int> pCropRect, int pMargin)
 {
     if (pCropRect.height && pCropRect.width)
     {
         _doCrop = true;
         _cropRect = pCropRect;
-        _margin = pMargin;
+        // We need enough margin to normalize descriptors at the border of the ROI
+        _margin = max(pMargin, max(_cellSize.width, _cellSize.height)) * max(_blockSize.width/2, _blockSize.height/2);
     }
 }
 
 /*************/
-void Descriptor_Hog::setHogParams(const cv::Size_<uint> pBlockSize, const cv::Size_<uint> pCellSize, const unsigned int pBinsPerCell, const bool pSigned)
+void Descriptor_Hog::setHogParams(const cv::Size_<int> pDescriptorSize, const cv::Size_<int> pBlockSize, const cv::Size_<int> pCellSize, const unsigned int pBinsPerCell, const bool pSigned)
 {
+    if (pDescriptorSize == cv::Size_<int>(0, 0) ||
+        pBlockSize == cv::Size_<int>(0, 0) ||
+        pCellSize == cv::Size_<int>(0, 0))
+    {
+        return;
+    }
+
+    _descriptorSize = pDescriptorSize;
+    _blockSize.width = (pBlockSize.width / 2)*2 + 1; // We ensure that we have odd values
+    _blockSize.height = (pBlockSize.height / 2)*2 + 1; // Same here
+    _cellSize = pCellSize;
+    _binsPerCell = pBinsPerCell;
+    _signed = pSigned;
+}
+
+/*************/
+float Descriptor_Hog::getDescriptorNorm(vector<float> pDescriptor) const
+{
+    float norm = _epsilon;
+
+    // This is a L1-sqrt norm
+    for (int i = 0; i < _binsPerCell; ++i)
+        norm += abs(pDescriptor[i]);
+
+    norm = sqrtf(norm);
+
+    return norm;
 }
--- src/blobtrainer.cpp
+++ src/blobtrainer.cpp
@@ -8,13 +8,13 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * switcher is distributed in the hope that it will be useful,
+ * blobserver is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with switcher.  If not, see <http://www.gnu.org/licenses/>.
+ * along with blobserver.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -39,11 +39,15 @@ using namespace std;
 
 static gboolean gVersion = FALSE;
 static gchar* gExtension = NULL;
+static gchar* gPositiveDir = NULL;
+static gchar* gNegativeDir = NULL;
 
 static GOptionEntry gEntries[] =
 {
     {"version", 'v', 0, G_OPTION_ARG_NONE, &gVersion, "Shows the version of this software", NULL},
     {"extension", 'e', 0, G_OPTION_ARG_STRING, &gExtension, "Specifies the extension for the files to load (default: png)", NULL},
+    {"positive", 'p', 0, G_OPTION_ARG_STRING, &gPositiveDir, "Specifies the subdirectory where to load positive images (default: ./positive)", NULL},
+    {"negative", 'n', 0, G_OPTION_ARG_STRING, &gNegativeDir, "Specifies the subdirectory where to load negative images (default: ./negative)", NULL},
     {NULL}
 };
 
@@ -69,28 +73,32 @@ int parseArgs(int argc, char** argv)
     }
 
     if (gExtension == NULL)
-    {
         gExtension = (gchar*)"png";
-    }
+
+    if (gPositiveDir == NULL)
+        gPositiveDir = (gchar*)"positive";
+
+    if (gNegativeDir == NULL)
+        gNegativeDir = (gchar*)"negative";
 
     return 0;
 }
 
 /*************/
-vector<string> loadFileList()
+vector<string> loadFileList(string pDirectory)
 {
     vector<string> fileList;
     GError* error;
     GDir* directory;
 
-    directory = g_dir_open((const gchar*)"./", 0, &error);
+    directory = g_dir_open((const gchar*)(string("./") + pDirectory).c_str(), 0, &error);
     const gchar* filename;
     while ((filename = g_dir_read_name(directory)) != NULL)
     {
         char* substr = strstr((char*)filename, (char*)gExtension);
         if (substr == filename + strlen((const char*)filename) - strlen((const char*)gExtension))
         {
-            string strName = string((const char*)filename);
+            string strName = string("./") + pDirectory + string("/") + string((const char*)filename);
             fileList.push_back(strName);
         }
     }
@@ -104,6 +112,100 @@ int main(int argc, char** argv)
     int result = parseArgs(argc, argv);
     if (result)
         return result;
+
+    // Load both valid and invalid file list
+    vector<string> positiveFiles = loadFileList(string(gPositiveDir));
+    vector<string> negativeFiles = loadFileList(string(gNegativeDir));
+
+    // Set up the descriptor
+    Descriptor_Hog descriptor;
+    int binsPerCell = 9;
+    descriptor.setHogParams(cv::Size_<int>(8, 16), cv::Size_<int>(3, 3), cv::Size_<int>(8, 8), binsPerCell, false);
+
+    // Set up training data
+    int nbrImg = 0;
+    vector<float> labels;
+    vector<float> trainingData;
+    for (int i = 0; i < positiveFiles.size(); ++i)
+    {
+        cout << "Analysing " << positiveFiles[i] << endl;
+        cv::Mat image = cv::imread(positiveFiles[i]);
+        descriptor.setImage(image);
+        vector<float> description = descriptor.getDescriptor(cv::Point_<int>(8, 8));
+
+        labels.push_back(1.f);
+        for (int j = 0; j < description.size(); ++j)
+            trainingData.push_back(description[j]);
+
+        nbrImg++;
+    }
+
+    for (int i = 0; i < negativeFiles.size(); ++i)
+    {
+        cout << "Analysing " << negativeFiles[i] << endl;
+        cv::Mat image = cv::imread(negativeFiles[i]);
+        descriptor.setImage(image);
+        for (int x = 0; x < 4; ++x)
+        {
+            for (int y = 0; y < 4; ++y)
+            {
+                vector<float> description = descriptor.getDescriptor(cv::Point_<int>(8 + x*16, 8 + y*16));
+
+                labels.push_back(-1.f);
+                for (int j = 0; j < description.size(); ++j)
+                    trainingData.push_back(description[j]);
+
+                nbrImg++;
+            }
+        }
+    }
+    
+    // Set up the SVM train parameter
+    CvSVMParams svmParams;
+    svmParams.svm_type = CvSVM::C_SVC;
+    svmParams.kernel_type = CvSVM::LINEAR;
+    svmParams.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 1e5, 1e-1);
+
+    // Train the SVM
+    CvSVM svm;
+    cv::Mat labelsMat((int)labels.size(), 1, CV_32FC1, &labels[0]);
+    cv::Mat trainingMat(nbrImg, (int)trainingData.size() / nbrImg, CV_32FC1, &trainingData[0]);
+    result = svm.train(trainingMat, labelsMat, cv::Mat(), cv::Mat(), svmParams);
+
+    svm.save("model.xml");
+
+    cout << endl << "--- Testing positive files ---" << endl;
+    int positive = 0;
+    int negative = 0;
+    for (int i = 0; i < positiveFiles.size(); ++i)
+    {
+        cv::Mat image = cv::imread(positiveFiles[i]);
+        descriptor.setImage(image);
+        vector<float> description = descriptor.getDescriptor(cv::Point_<int>(8, 8));
+        cv::Mat descriptionMat(1, (int)description.size(), CV_32FC1, &description[0]);
+        float value = svm.predict(descriptionMat);
+        cout << positiveFiles[i] << " -> " << value << endl;
+
+        if (value == 1)
+            positive++;
+    }
+
+    cout << endl << "--- Testing negative files ---" << endl;
+    for (int i = 0; i < negativeFiles.size(); ++i)
+    {
+        cv::Mat image = cv::imread(negativeFiles[i]);
+        descriptor.setImage(image);
+        vector<float> description = descriptor.getDescriptor(cv::Point_<int>(8, 8));
+        cv::Mat descriptionMat(1, (int)description.size(), CV_32FC1, &description[0]);
+        float value = svm.predict(descriptionMat);
+        cout << negativeFiles[i] << " -> " << value << endl;
+
+        if (value == -1)
+            negative++;
+    }
+
+    cout << endl << "Positive: " << positive << " / " << positiveFiles.size() << endl;
+    cout << endl << "Negative: " << negative << " / " << negativeFiles.size() << endl;
 
     return 0;
 }
