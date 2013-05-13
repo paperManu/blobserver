8963e566b52bcc019cfabe4c3e3379b0ffd2c948
Emmanuel Durand <emmanueldurand@gmail.com>
2013-05-13 16:53:25 -0400

    Updated documentation

--- include/mainpage.h
+++ include/mainpage.h
@@ -27,33 +27,55 @@
 
 /*! \mainpage Blobserver
  * 
+ **************
  * \section intro_sec What is Blobserver
  * 
  * Blobserver is an OSC-based server aimed at detecting entities (objects / people / light / ...), in any compatible image flow. Its structure is so that it should be relatively easy to add new detectors as well as new light sources. As of yet, configuration and communication with Blobserver is done entirely through OSC messaging. Some kind of configuration file will be added soon to simplify the setup and usability, especially for permanent installations.
  * 
  * Blobserver is built around the concept of flow. A flow is the association of a detector and as many sources as needed for it to work correctly. At each frame, all the flows are evaluated, and the various objects detected are sent through OSC to the corresponding clients.
  * 
+ **************
  * \section sources_sec List of compatible sources
  * 
  * The following sources are compatible with Blobserver:
  * - any source compatible with OpenCV
- * - shmdata will be added very soon
+ * - shmdata sources
+ *
+ * Some parameters are available for all kind of sources, none if these transformations are activated by default:
+ * - mask (string): file path to the image file to use as a mask
+ * - autoExposure (int[6]): parameters for auto exposure, measured in a specified area. Parameters are: [x] [y] [width] [height] [target] [margin].
+ * - scale (float, default 1.0): apply scaling on the image
+ * - rotation (float): apply rotation on the image, in degrees
+ * - noiseFiltering (int, default 0): set to 1 to activate noise filtering
+ * - distortion (int[3]): distortion correction (see http://wiki.panotools.org/Lens_correction_model). Parameters are: [a] [b] [c]
+ * - vignetting (int[3]): correction of the vignetting (see http://lensfun.berlios.de/lens-calibration/lens-vignetting.html). Parameters are: [k1] [k2] [k3]
+ * - iccInputProfile (string): file path to an ICC profile (for color correction)
+ * - hdri (int[3]): activates the creation of a HDR image. Parameters are: [startExposure] [stepSize] [nbrSteps].
+ * - save (int[2] string): activates the automatic save of grabs. Parameters are: [activation] [period] [filename] 
  * 
  * \subsection source_opencv_sec OpenCV sources (Source_OpenCV)
  * 
  * Note that OpenCV must have been compiled with the desired camera support.
  * 
  * Available parameters:
- * - width
- * - height
- * - framerate
- * - cameraNumber: camera index as per OpenCV numbering. Setting this to 0 will use the first camera found.
+ * - cameraNumber (int): camera index as per OpenCV numbering. Setting this to 0 will use the first camera found.
+ * - width (int)
+ * - height (int)
+ * - framerate (int)
+ * - exposureTime (int): exposure time, conversion to ms is dependent of the camera model
+ * - gain (int): gain applied to the camera sensor output, conversion to dB is dependent of the camera model
+ * - gamma (int): gamma applied to the image, multiplied by 1024
+ * - whiteBalanceRed (int): coefficient applied to the red channel, multiplied by a value dependent of the camera model
+ * - whiteBalanceBlue (int): coefficient applied to the blue channel, multiplied by a value dependent of the camera model
+ * - iso (int): link speed to set for firewire cameras
  * 
  * \subsection source_shmdata_sec shmdata sources (Source_Shmdata)
  * 
  * Available parameters:
- * - location (location of the shmdata file)
+ * - location (string): file path to the shmdata
+ * - cameraNumber (int): index of the shmdata, this can be used to access multiple times to the same shmdata
  * 
+ **************
  * \section detectors_sec List of detectors
  * 
  * \subsection detector_lightspot_sec Lighspots (Detector_LightSpots)
@@ -63,10 +85,10 @@
  * Number of source(s) needed: 1
  * 
  * Available parameters: 
- * - detectionLevel (default 2): minimum level (as a multiple of the standard deviation) to consider a point as a light spot.
- * - filterSize (default 3): Size of the kernel for the morphological operations (to smooth the noise).
- * - processNoiseCov (default 1e-5): Noise of the movement of the tracked object. Used for filtering detection.
- * - measurementNoiseCov (default 1e-5): Noise of the measurement (capture + detection) of the tracked object. Used for filtering detection.
+ * - detectionLevel (int, default 2): minimum level (as a multiple of the standard deviation) to consider a point as a light spot.
+ * - filterSize (int, default 3): Size of the kernel for the morphological operations (to smooth the noise).
+ * - processNoiseCov (int, default 1e-5): Noise of the movement of the tracked object. Used for filtering detection.
+ * - measurementNoiseCov (int, default 1e-5): Noise of the measurement (capture + detection) of the tracked object. Used for filtering detection.
  * 
  * OSC output:
  * - name: lightSpots
@@ -79,15 +101,39 @@
  * Number of source(s) needed: 1
  * 
  * Available parameters:
- * - detectionLevel (default 2): minimum level (as a multiple of the standard deviation) to consider a point as a light spot.
- * - filterSize (default 3): Size of the kernel for the morphological operations (to smooth the noise).
- * - processNoiseCov (default 1e-6): Noise of the movement of the tracked object. Used for filtering detection.
- * - measurementNoiseCov (default 1e-4): Noise of the measurement (capture + detection) of the tracked object. Used for filtering detection.
+ * - detectionLevel (int, default 2): minimum level (as a multiple of the standard deviation) to consider a point as a light spot.
+ * - filterSize (int, default 3): Size of the kernel for the morphological operations (to smooth the noise).
+ * - processNoiseCov (int, default 1e-6): Noise of the movement of the tracked object. Used for filtering detection.
+ * - measurementNoiseCov (int, default 1e-4): Noise of the measurement (capture + detection) of the tracked object. Used for filtering detection.
  * 
  * OSC output:
  * - name: meanOutliers
  * - values: X(int) Y(int) Size(int) dX(int) dY(int)
+ *
+ * \subsection detector_hog_sec Histogram of Oriented Gradients (Detector_Hog)
+ *
+ * This detector searches for objects corresponding to the model trained with blobtrainer.
+ *
+ * Number of source(s) needed: 1
+ *
+ * Available parameters:
+ * - modelFilename (string): path to the file model
+ * - maxTimePerFrame (int, default 1e5): maximum time (in us) to allow for detection at each frame
+ * - maxThreads (int, default 4): maximum number of threads to use
+ * - mergeDistance (int, default 64): distance below which detected blobs are merged
+ * - filterSize (int, default 3): size of the morphological filter used to filter noise in the background subtraction pass
+ * - roiSize (int[2], default 64 128): size (in pixels) of the detection window
+ * - blockSize (int[2], default 2 2): size (in cells) where normalization is applied
+ * - cellSize (int[2], default 16 16): size (in pixels) of a histogram of oriented gradients cell
+ * - bins (int, default 9): number of orientations to consider
+ * - margin (float, default 0.0): margin to the hyperplane to add to the detection (higher = less false positives and less hit rate)
+ * - lifetime (int, default 30): time (in frames) during which a blob is kepts even if not detected
+ * - processNoiseCov (int, default 1e-6): noise of the movement of the tracked object. Used for filtering detection.
+ * - measurementNoiseCov (int, default 1e-4): noise of the measurement (capture + detection) of the tracked object. Used for filtering detection.
+ * - saveSamples (int, default 0): if set to 1, saves detected samples older than saveSamplesAge
+ * - saveSamplesAge (int, default 120): see saveSamples
  * 
+ **************
  * \section howto_xml_sec How to use Blobserver - Configuration through a XML file
  * 
  * The easiest way to configure Blobserver is to use a configuration file which is loaded at launch or sent to blobserver with blobcontroller, a small utility dedicated to this purpose. A few examples are shipped with the source code, and here follows the general form of a blobserver configuration file.
@@ -139,6 +185,7 @@
  * </Blobserver>
  * \endcode
  * 
+ **************
  * \section howto_osc_sec How to use Blobserver - Configuration through OSC messages
  * 
  * Blobserver is entirely configurable through OSC messages and listens on port 9002 (by default). Here are the available message.
@@ -286,6 +333,7 @@
  * - client (string): ip address or network name for the client the Blobserver sends messages to.
  * - sourceName: name of the source as given by a call to "/blobserver/sources client"
  * 
+ **************
  * \section howto_messages_sec How to use Blobserver - Detection related messages
  * 
  * During each iteration of the main loop, detected objects are sent through OSC to all clients which subscribed to each flow. Messages can vary depending on the detector used, and you should report to the section dedicated to this detector for further information. Anyway, these messages have the following general form:
