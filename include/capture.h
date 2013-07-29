368a43111b346fdd1a9472dc58bed6c44a349f61
Emmanuel Durand <emmanueldurand@gmail.com>
2013-07-29 16:48:53 -0400

    Added a new Capture_3D_PclRgba type, meant to contain color point clouds

--- include/capture.h
+++ include/capture.h
@@ -27,8 +27,14 @@
 
 #include <memory>
 
+#include "config.h"
+
 #include <glib.h>
 #include <opencv2/opencv.hpp>
+#if HAVE_PCL
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#endif // HAVE_PCL
 
 /*************/
 class Capture
@@ -54,6 +60,23 @@ class Capture_2D_Mat : public Capture
     private:
         cv::Mat mBuffer;
 };
+
+#if HAVE_PCL
+/*************/
+class Capture_3D_PclRgba : public Capture
+{
+    public:
+        Capture_3D_PclRgba() {};
+        Capture_3D_PclRgba(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr input) {mPcl = input;}
+
+        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr get() {return mPcl;}
+
+        std::string type() {return std::string("Capture_3D_Pcl");}
+
+    private:
+        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr mPcl;
+};
+#endif //HAVE_PCL
 
 typedef std::shared_ptr<Capture> Capture_Ptr;
 typedef std::shared_ptr<Capture_2D_Mat> Capture_2D_Mat_Ptr;
